4401ad915c1c92c8d772340e05038cfa0164828d
axtyax <axtyab@gmail.com>
2018-07-22 12:02:17 -0400

    d

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.sendFile(path.join(__dirname, './client/public', 'index.html'));
 });
-
-router.post('/', function(req, res, next) {
-	console.log("hi");
-	res.render('index', { title: 'Express' });
-});
-
 
 module.exports = router;
