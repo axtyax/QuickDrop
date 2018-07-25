9aaae6e448731b903e844726d38d9dd4263614a0
axtyax <axtyab@gmail.com>
2018-07-25 09:45:55 -0400

    d

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,9 +13,14 @@ class App extends Component {
 				<link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet" />
 
 				<h1 id="main-title"> QuickDrop </h1>
-				<p id="title-caption"> Easy-to-use large temporary file storage </p>
+				<p class="caption"> Easy-to-use temporary file storage </p>
 
 				<DropZone />
+
+				<h1 class="bottom-caption">
+					QuickDrop stores up to 50mb of files for 12 hours. To retrieve a uploaded files, just hit 'Upload'
+					and copy the paste the returned link into your browser. 
+				</h1>
 
 			</div>
     );
